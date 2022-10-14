4e7d5169d21aae388d8c8b23b4e40a7b795cea26
Stefan Seifert <stefanseifert@users.noreply.github.com>
2022-10-14 22:32:50 +0200

    pick difficulty level

--- src/components/setup/DifficultyLevel.vue
+++ src/components/setup/DifficultyLevel.vue
@@ -6,11 +6,16 @@
       <label for="difficultyLevel" class="form-label">{{t('setup.difficultyLevel.easy')}}</label>
     </div>
     <div class="col-8 col-md-4">
-      <input type="range" class="form-range" min="1" max="3" id="difficultyLevel"
+      <input type="range" class="form-range" min="1" max="6" id="difficultyLevel"
           :value="$store.state.setup.difficultyLevel" @input="updateDifficultyLevel($event)">
     </div>
     <div class="col-1">
       <label for="difficultyLevel" class="form-label">{{t('setup.difficultyLevel.hard')}}</label>
+    </div>
+  </div>
+  <div class="row">
+    <div class="offset-1 col-8 col-md-4 text-muted small">
+      {{t(`difficultyLevel.${$store.state.setup.difficultyLevel}`)}}
     </div>
   </div>  
 
