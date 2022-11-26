38c1ec4f643c1f3d7e9794b1c03fc12d787137b8
Stefan Seifert <stefanseifert@users.noreply.github.com>
2022-11-26 10:16:43 +0100

    Fix issue with restoring latest view when reopening the app

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import { RouteRecordRaw } from 'vue-router'
 import AppHome from '@/views/AppHome.vue'
 import NotFound from '@/views/NotFound.vue'
 import SetupGame from '@/views/SetupGame.vue'
@@ -6,6 +6,7 @@ import RoundPlayer from '@/views/RoundPlayer.vue'
 import RoundBot from '@/views/RoundBot.vue'
 import SetupARNO from '@/views/SetupARNO.vue'
 import Scoring from '@/views/Scoring.vue'
+import createRouter from "brdgm-commons/src/util/router/createRouter"
 
 const LOCALSTORAGE_KEY = process.env.VUE_APP_LOCALSTORAGE_KEY_PREFIX + "route"
 
@@ -47,22 +48,4 @@ const routes: Array<RouteRecordRaw> = [
   }
 ]
 
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes
-})
-
-// store last used route path in local storage
-router.afterEach(to => {
-  localStorage.setItem(LOCALSTORAGE_KEY, to.fullPath)
-})
-// redirect to lase used route path
-let isFirstTransition = true
-router.beforeEach((to, _from, next) => {
-  const lastRouteFullPath = localStorage.getItem(LOCALSTORAGE_KEY)
-  if (to.name === "Home" && lastRouteFullPath && isFirstTransition) next(lastRouteFullPath)
-  else next()
-  isFirstTransition = false
-})
-
-export default router
+export default createRouter(routes, LOCALSTORAGE_KEY, 'AppHome')
