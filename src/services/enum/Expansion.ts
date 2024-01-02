dc61bb1a2c27070c926dc0c6aff26b9fbc0602dd
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-01-02 18:31:56 +0100

    fix quote

--- src/services/enum/Expansion.ts
+++ src/services/enum/Expansion.ts
@@ -2,7 +2,7 @@
  * Expansion
  */
 enum Expansion {
-  ARNO_CONSERVATION_PROJECT_MODULE = "arno-conservation-project-module",
-  MARINE_WORLDS = "marine-worlds"
+  ARNO_CONSERVATION_PROJECT_MODULE = 'arno-conservation-project-module',
+  MARINE_WORLDS = 'marine-worlds'
 }
 export default Expansion
