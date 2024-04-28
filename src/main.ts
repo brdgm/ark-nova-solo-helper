dc5eb0955fea3d59a290599d4e344ad7badc192f
Stefan Seifert <stefanseifert@users.noreply.github.com>
2024-04-28 22:55:17 +0200

    switch to pinia

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import piniaPluginPersistedState from 'pinia-plugin-persistedstate'
-import { store, key } from './store'
 import router from './router'
 import i18n from './i18n'
 import App from './App.vue'
@@ -14,7 +13,6 @@ const pinia = createPinia()
 
 createApp(App)
   .use(pinia)
-  .use(store, key)
   .use(router)
   .use(i18n)
   .mount('#app')
